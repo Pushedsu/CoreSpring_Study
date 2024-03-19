2840795b3c355547b41a6d6152b216b23342974b
민수 <109027302+Pushedsu@users.noreply.github.com>
2024-03-19 20:49:14 +0900

    컴포넌트 스캔

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,10 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
-
+@Component
 public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long, Member> store = new HashMap<>(); //실무에서는 컨커런트 해쉬 맵을 사용!! 동시성 문제 때문이다.
     @Override
